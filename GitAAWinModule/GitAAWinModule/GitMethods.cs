43d5e56a0bef1c66db8626704edda111cebe3c1e
jasonhuber <jasonhuber@gmail.com>
2009-03-06 13:49:45 -0700

    Initially have it working. Git is on it's own menu.

---
+++ GitAAWinModule/GitAAWinModule/GitMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sage.Platform.Application.UI;
+using Sage.Platform.Application;
+using Microsoft.Practices.ObjectBuilder;
+
+
+namespace GitAAWinModule
+{
+    public class GitMethods : ModuleInit<UIWorkItem>, IModuleConfigurationProvider
+    {
+        protected override void Load()
+        {
+            base.Load();
+        }
+        public override string ToString()
+        {
+            return "My First Module";
+        }
+        #region ImoduleConfigrationProvider Members
+        public ModuleConfiguration GetConfiguration()
+        {
+            return ModuleConfiguration.LoadFromResource("GitAAWinModule.module.xml", GetType().Assembly);
+        }
+        #endregion
+
+    }
+}
